36f225765c4639fa20d3cfac919d400bbb058928
czkiii <czki.adam@gmail.com>
2026-08-18 22:26:52 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,21 +1,19 @@
 MOON DOGE
 
 Cseréld le a GitHub repóban az azonos nevű fájlokat:
-- index.html
-- manifest.webmanifest
-- sw.js
-- icon-192.png
-- icon-512.png
+index.html
+manifest.webmanifest
+sw.js
+icon-192.png
+icon-512.png
 
-Irányítás:
-- auto-jump
-- tilt balra/jobbra
-- BOOST levegőben, nyomva tartva tölt, elengedésre aktivál
-
-A frissítés tartalma:
-- ritkább, karakteresebb landolási pontok
-- zónánként más platformobjektumok
-- lassabb, hosszabb pacing
-- finomabb kamera
-- új Moon approach
-- valódi Moon completion
+Új:
+- hosszabb run
+- Moon checkpoint, utána Mars
+- billenő platform
+- villámfelhő
+- meteor shower warninggal
+- repülő hazard
+- madárraj
+- széllökés
+- difficulty director
--- sw.js
+++ sw.js
@@ -1,20 +1,8 @@
-const CACHE='moon-doge-live-polished';
+const CACHE='moon-doge-chaos';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
-    return;
-  }
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-    return r;
-  }).catch(()=>caches.match(e.request)));
+  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));return}
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))
 });
