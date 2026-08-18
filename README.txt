9761c1fd247b2e689d3b0cdb1ef609278737f83c
czkiii <czki.adam@gmail.com>
2026-08-18 22:13:00 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -7,16 +7,15 @@ Cseréld le a GitHub repóban az azonos nevű fájlokat:
 - icon-192.png
 - icon-512.png
 
-Új irányítás:
-- A Shiba automatikusan ugrik minden landolás után.
-- Telefon döntése balra / jobbra = irányítás.
-- BOOST levegőben tölthető; elengedésre extra lökést ad.
-- iPhone Safari az első PLAY-nél kérhet motion/orientation engedélyt.
+Irányítás:
+- auto-jump
+- tilt balra/jobbra
+- BOOST levegőben, nyomva tartva tölt, elengedésre aktivál
 
-GitHub Pages: main / (root)
-
-Ha a főképernyős PWA régi marad:
-1. nyisd meg Safari-ban az oldalt,
-2. frissíts rá,
-3. zárd be a főképernyős appot teljesen,
-4. nyisd meg újra.
+A frissítés tartalma:
+- ritkább, karakteresebb landolási pontok
+- zónánként más platformobjektumok
+- lassabb, hosszabb pacing
+- finomabb kamera
+- új Moon approach
+- valódi Moon completion
--- sw.js
+++ sw.js
@@ -1,31 +1,20 @@
-const CACHE='moon-doge-live-gyro-auto';
+const CACHE='moon-doge-live-polished';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-
 self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
 self.addEventListener('fetch',e=>{
   if(e.request.mode==='navigate'){
-    e.respondWith(
-      fetch(e.request).catch(()=>caches.match('./index.html'))
-    );
+    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
     return;
   }
-  e.respondWith(
-    fetch(e.request).then(r=>{
-      const copy=r.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-      return r;
-    }).catch(()=>caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(e.request)));
 });
